b871328b04a97598f507118e021c5b39c41dced3
Thao Pham <phamthaop.9@gmail.com>
2018-11-05 00:51:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    puts "NO! NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else 
