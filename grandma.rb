fe217cd2d6082bce1bead9e076e3d7e39cdfaced
Thao Pham <phamthaop.9@gmail.com>
2018-11-05 01:06:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  if phrase == phrase.upcase && phrase !=="I LOVE YOU GRANDMA!"
+  if phrase == phrase.upcase && phrase !="I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
